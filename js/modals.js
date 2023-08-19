41fb61e909476eaead65dc241934e6ca7e132f09
Olga Smirnova <almazkacode@gmail.com>
2023-08-19 15:39:07 +0600

    feat: add the appearance and disappearance of modal windows

---
+++ js/modals.js
@@ -0,0 +1,40 @@
+const modalButtons = document.querySelectorAll('.btn');
+const modalOverlay = document.querySelector('.modals__overlay');
+const modals = document.querySelectorAll('.modal');
+
+const closeButtons = document.querySelectorAll('.modal-close');
+
+modalButtons.forEach((btn) => {
+	btn.addEventListener('click', (event) => {
+    profileMenu.classList.remove('profile-enter__menu--active');
+		let currentBtn = event.currentTarget.getAttribute('data-btn');
+
+		modals.forEach((item) => {
+			item.classList.remove('modal--visible');
+		});
+
+    console.log(document.querySelector(`[data-modal="${currentBtn}"]`));
+
+    document.querySelector(`[data-modal="${currentBtn}"]`).classList.add('modal--visible');
+		modalOverlay.classList.add('modals__overlay--visible');
+	});
+});
+
+function closeModalWindow() {
+  modalOverlay.classList.remove('modals__overlay--visible');
+    modals.forEach((item) => {
+			item.classList.remove('modal--visible');
+		});
+}
+
+modalOverlay.addEventListener('click', (event) => {
+	if (event.target == modalOverlay) {
+		closeModalWindow()
+	}
+});
+
+closeButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    closeModalWindow()
+  })
+})
